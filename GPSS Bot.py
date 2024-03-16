466ac9649b2403fa8ea69ed8c3f901e15b4a4e63
Daniil Savchenko <95571784+LigindaLeg@users.noreply.github.com>
2024-03-17 00:35:04 +0400

    Update GPSS Bot.py
    Добавлена команда GPSS!info
    Добавлена команда GPSS!list

--- GPSS Bot.py
+++ GPSS Bot.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import CommandOnCooldown
+import asyncio
 
 intents = discord.Intents.all()
 intents.members = True
@@ -55,5 +57,52 @@ async def on_member_update(before, after):
         if str(after.id) in banned_ids:
             await after.ban(reason="Замечен ГОБС")
             await after.send("Вы были забанены ГОБС, подать аппеляцию тут - https://discord.gg/vkPQm4M3mT")
+@bot.command(name='info')
+@commands.cooldown(1, 300, commands.BucketType.user)  # Кулдаун на 5 минут
+async def info(ctx):
+    await ctx.send(
+        "Информация о боте <@1218231384024289392>\n"
+        "Данный бот автоматизирует блокировку участников, которые когда-либо участвовали в рейдах, ДДОС-Атаках и т.д.\n\n"
+        "Этот бот помогает Администраторам Дискорд-Серверов предотвращать рейды и другой возможный ущерб дискорд-серверам, "
+        "автоматизируя отбор участников и блокируя их\n\n"
+        "Создатель данного бота <@612288780107382795>\n\n"
+        "Все вопросы принимаются на нашем дискорд-сервере: https://discord.gg/S4ykbjaRWK"
+    )
+
+# Обработка ошибки при попытке использовать команду во время кулдауна
+@info.error
+async def info_error(ctx, error):
+    if isinstance(error, CommandOnCooldown):
+        await ctx.message.delete()
+
+# Функция для чтения данных из файла и форматирования строк
+def read_ids_from_file(filename):
+    with open(filename, 'r') as file:
+        ids = file.readlines()
+        return [f"<@{id.strip()}>" for id in ids]
+
+# Команда list для вывода данных из файла
+@bot.command()
+async def list(ctx):
+    ids = read_ids_from_file('ids.txt')
+    await ctx.send('\n'.join(ids))
+
+# Обработчик события on_message для кулдауна
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    # Проверяем, если сообщение начинается с префикса и это команда list
+    if message.content.startswith('!list'):
+        cooldown_seconds = 300  # 5 минут в секундах
+
+        # Проверяем, есть ли кулдаун для этого пользователя
+        if not hasattr(bot, 'last_list_command') or bot.last_list_command + cooldown_seconds < message.created_at.timestamp():
+            bot.last_list_command = message.created_at.timestamp()
+            await bot.process_commands(message)
+        else:
+            await message.delete()
+    await bot.process_commands(message)
 
 bot.run('token')
